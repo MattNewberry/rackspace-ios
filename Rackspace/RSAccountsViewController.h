4c83b776958e95700eb38caeedf5f4ba43ed1c58
Mike Mayo <greenisus@gmail.com>
2011-09-27 13:48:58 -0700

    remove nimbus, start accounts view controller, and fix organization name in project

---
+++ Rackspace/RSAccountsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RSAccountsViewController.h
+//  Rackspace
+//
+//  Created by Michael Mayo on 9/27/11.
+//  Copyright (c) 2011 Rackspace, US Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAccountsViewController : UITableViewController
+
+@end
---
+++ Rackspace/RSAccountsViewController.m
@@ -0,0 +1,13 @@
+//
+//  RSAccountsViewController.m
+//  Rackspace
+//
+//  Created by Michael Mayo on 9/27/11.
+//  Copyright (c) 2011 Rackspace, US Inc. All rights reserved.
+//
+
+#import "RSAccountsViewController.h"
+
+@implementation RSAccountsViewController
+
+@end
