2e7171aa8efc76344505774881c12890a0bb931e
Mike Mayo <greenisus@gmail.com>
2011-10-10 12:08:44 -0700

    update mogen script

--- mogen.sh
+++ mogen.sh
@@ -1,1 +1,1 @@
-mogenerator -m ./Rackspace/Rackspace.xcdatamodeld/Rackspace.xcdatamodel/ -O ./Rackspace/Models/ --base-class=RSRecord --template-path ./mogenerator-templates/
+mogenerator -m ./Rackspace/Rackspace.xcdatamodeld/ -O ./Rackspace/Models/ --base-class=RSRecord --template-path ./mogenerator-templates/
