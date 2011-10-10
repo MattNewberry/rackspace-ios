bcb437498491e6f120a1357cee6d8d49b3e3c5d5
Mike Mayo <greenisus@gmail.com>
2011-10-10 11:42:31 -0700

    get image details

--- Rackspace/Models/RSImage.m
+++ Rackspace/Models/RSImage.m
@@ -12,6 +12,18 @@
     return [self serversAPIRequestForGet];
 }
 
+//+ (CKRequest *)requestForPost {
+//
+//    CKRequest *request = [CKRequest requestWithMap:[self mapForRequestMethod:CKRequestMethodPOST]];
+//    
+////    [request setBodyWithJSONDict:<#(NSDictionary *)#>
+//    
+//    
+//    request.body = [self serialize];
+//    return request;
+//    
+//}
+
 - (NSString *)description {
     return $S(@"Image %@: %@", self.id, self.name);
 }
