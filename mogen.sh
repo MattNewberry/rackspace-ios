7fbbf8f83e0ff8721bf689c3e85a8c0cf2843f67
Mike Mayo <greenisus@gmail.com>
2011-10-11 10:45:59 -0700

    fix mogen script

--- mogen.sh
+++ mogen.sh
@@ -1,1 +1,2 @@
-mogenerator -m ./Rackspace/Rackspace.xcdatamodel/ -O ./Rackspace/Models/ --base-class=CKRecord --template-path ./mogenerator-templates/
+rm ./Rackspace/Models/_*
+mogenerator -m ./Rackspace/Models/Rackspace.xcdatamodel/ -O ./Rackspace/Models/ --base-class=CKRecord --template-path ./mogenerator-templates/
